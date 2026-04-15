0587caef71e5d6be9b6c2114ffe61f8a1b937d86
2024129041 <b01024403893@gmail.com>
2026-04-15 16:47:43 +0900

    Update main.c

---
+++ p2/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int N = 0, engcount = 0, intcount = 0, max_engcount = 0, max_intcount = 0;
+    char ch = ' ';
+    scanf("%d", &N);
+    for(int i = 1;i <= N;i++){
+        scanf("%c", &ch);
+        if (ch >= 'a' && ch <= 'z') {
+            intcount = 0;
+            ++engcount;
+            if (engcount > max_engcount) max_engcount = engcount;
+        }
+        else {
+            engcount = 0;
+            ++intcount;
+            if (intcount > max_intcount) max_intcount = intcount;
+        }
+    }
+    printf("%d\n", max_engcount);
+    printf("%d", max_intcount);
+    return 0;
+}
