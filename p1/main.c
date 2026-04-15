b226664a47071fc70deca27a36a89704a5d3b64c
2024129041 <b01024403893@gmail.com>
2026-04-15 15:40:47 +0900

    Update main.c

--- p1/main.c
+++ p1/main.c
@@ -3,13 +3,15 @@
 
 int main(void)
 {
-    int correct = 0, answer = 0;
+    int correct = 0, answer = 0, count = 0;
     scanf("%d", &correct);
     do {
         scanf("%d", &answer);
         if(answer > correct) printf("%d>?\n", answer);
         else if(answer < correct) printf("%d<?\n", answer);
-        else printf("%d==?", answer, correct);
+        else printf("%d==?\n", answer, correct);
+        count++;
     } while(answer != correct);
+    printf("%d", count);
     return 0;
 }
